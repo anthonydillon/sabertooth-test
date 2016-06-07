bb5102a24bd1c97a8e1cc6cf275867e03cff9f74
Anthony Dillon <me@anthonydillon.com>
2016-06-07 10:32:35 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const BluetoothDevice = require('web-bluetooth')
+
+const device = new BluetoothDevice({
+  name: 'Moto 360 3G3Z',
+  service: ['battery_level']
+})
+
+device.connect().then(device => {
+  console.log(device, 'Connected!');
+  getBatteryLevel();
+}).catch(error => {
+  console.log(error);
+});
+
+const getBatteryLevel() = function () {
+  device.getValue('battery_service')
+         .then(value => {
+            console.log('Battery level', value);
+         });
+}
